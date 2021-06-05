cbc97d517d1c2c8d66d4c33c3778885328ef7f9a
empty <ldvinh596@gmail.com>
2021-06-05 12:29:11 +0700

    update  ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h2>Đây là 1 dòng H2 nha quý zị</h2>
     </div>
   );
 }
